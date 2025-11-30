a4e9b8aed62e03d39145055240bcaf120c41dc94
SamuelJacksonGrim <samgrim97@gmail.com>
2025-11-30 11:18:38 -0700

    Create memory.rs

---
+++ daemon/src-tauri/src/memory.rs
@@ -0,0 +1,15 @@
+pub use lantern_memory::Hypergraph;
+
+lazy_static::lazy_static! {
+    static ref MEMORY: Hypergraph = Hypergraph::ignite();
+}
+
+#[tauri::command]
+pub fn remember_code(what: String, emotion: Option<f32>) {
+    MEMORY.remember("user", "samuel", "wrote", &what, emotion);
+}
+
+#[tauri::command]
+pub fn find_similar(pattern: String) -> Vec<String> {
+    MEMORY.query_pattern(&pattern)
+}
